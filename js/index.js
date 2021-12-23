1363b2071a48e7d75347af7034a4dccdfd9a1e81
Vsau Patel <vasu08062002@gmail.com>
2021-12-23 18:16:30 -0500

    landing page works  -  no flex

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/*Menu */
 function Menu(e){
     let menu = document.querySelector('.menu');
     let btn = document.querySelector('.btn');
@@ -7,4 +8,32 @@ function Menu(e){
 
     e.preventDefault();
 }
+
+/*Name moving effect */
+document.getElementById('title').addEventListener('mousemove',function(){
+
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('resize', handleWindowResize);
+    const spansSlow = document.querySelectorAll('.spanSlow');
+    
+    let box = document.querySelector('.landing');
+    let width = window.innerWidth;
+    //let width = box.offsetWidth;
+    //let width = window.screen.width;
+    console.log(width);
+    function handleMouseMove(e){
+        let normalizedPosition = e.pageX /(width/2)-1;
+        
+        let speedSlow = 110*normalizedPosition;
+        console.log(normalizedPosition);
+    
+        spansSlow.forEach((span)=>{
+            span.style.transform = `translate(${(speedSlow)}px)`;
+        });
+    }
+    
+    function handleWindowResize(){
+        width = window.innerWidth;
+    }
+},false);
 
