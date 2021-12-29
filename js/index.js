a8b663eaaf3ac38356bc4cf51126c19580b07a76
Vasu Patel <vasu08062002@gmail.com>
2021-12-28 19:06:33 -0500

    Basic Website Works - First Commit

--- js/index.js
+++ js/index.js
@@ -36,3 +36,24 @@ document.getElementById('title').addEventListener('mousemove',function(){
     }
 },false);
 
+
+function checkElementLocation() {
+    var $window = $(window);
+    var bottom_of_window = $window.scrollTop() + $window.height();
+  
+    $('.image').each(function(i) {
+      var $that = $(this);
+      var bottom_of_object = $that.position().top + $that.outerHeight();
+  
+      //if element is in viewport, add the animate class
+      if (bottom_of_window > bottom_of_object) {
+        $(this).addClass('fadeIn');
+      }
+    });
+}
+// if in viewport, show the animation
+checkElementLocation();
+
+$(window).on('scroll', function() {
+    checkElementLocation();
+});
